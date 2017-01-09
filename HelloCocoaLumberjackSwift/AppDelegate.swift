0d7f5e884f85b9b41b2a74e6a30b9636f7ff9f04
Thomas B Homburg <thomas@houseofcode.io>
2017-01-09 21:24:55 +0100

    Initial commit

--- HelloCocoaLumberjackSwift/AppDelegate.swift
+++ HelloCocoaLumberjackSwift/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CocoaLumberjack
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        DDLog.add(DDTTYLogger.sharedInstance())
+        DDLog.add(DDASLLogger.sharedInstance())
+        
+        print("Hellooo")
+        DDLogDebug("Hellooo cocoa lumberjack")
+        
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-HelloCocoaLumberjackSwiftUITests/Pods-HelloCocoaLumberjackSwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloCocoaLumberjackSwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HelloCocoaLumberjackSwiftUITests
+@end
